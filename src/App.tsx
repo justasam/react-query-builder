0a204533b46a4633775d67f2012f4c56d71edae4
Justas Samuolis <samuolis.just@gmail.com>
2023-09-13 19:59:32 +0300

    Change margin

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@ import { baseConfig } from "mocks";
 function App() {
   return (
     <ChakraProvider>
-      <Container maxW="4xl" borderWidth="2px" borderRadius="lg" p="8" mt="8">
+      <Container
+        maxW="4xl"
+        borderWidth="2px"
+        borderRadius="lg"
+        p="8"
+        mt="8"
+        mb="8"
+      >
         <QueryBuilderProvider baseConfig={baseConfig}>
           <TableSelect />
           <Spacer mt="8" />
